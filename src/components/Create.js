cdeefb06e8d14bf2072792ad3e121d405ed2dee1
Simo-Pekka Varis <simopekka.varis@gmail.com>
2023-02-08 14:36:54 +0200

    create.js

---
+++ src/components/Create.js
@@ -0,0 +1,43 @@
+import { useState, Button } from 'react';
+  
+
+
+function Create() {
+
+    const [item, setItem ] = useState({
+        name:'', description: '', picture:''
+    });
+
+    const handleInputChange = (event) => {
+        setItem({...item, [event.target.name]: event.target.value});
+    }
+    
+    const save = () => {
+        console.log(item);
+    }
+
+
+    return (
+        <div>
+            <form onSubmit={save}>
+                <p>name:</p>
+                <input 
+                name="name"
+                value={item.name}
+                onChange={e => handleInputChange(e)}
+                />
+                <p>description:</p>
+                <input 
+                name="description"
+                value={item.description}
+                onChange={e => handleInputChange(e)}
+                />
+
+                <input type="submit" value="Submit" />
+                
+            </form>
+        </div>
+    )
+}
+
+export default Create;
